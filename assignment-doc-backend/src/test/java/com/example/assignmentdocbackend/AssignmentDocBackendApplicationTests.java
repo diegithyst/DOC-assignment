0337882b8ac4c1bd7ed5edccae3ac79c5c346ca1
Maria B <331289@viauc.dk>
2024-06-17 00:21:07 +0200

    initial commit

---
+++ assignment-doc-backend/src/test/java/com/example/assignmentdocbackend/AssignmentDocBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignmentdocbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentDocBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
